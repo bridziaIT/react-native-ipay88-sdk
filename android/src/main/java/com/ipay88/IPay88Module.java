b3915b1b29651425466e158ce368d3c0162ea4a0
xiaoleonex <37250577+xiaoleonex@users.noreply.github.com>
2022-02-28 14:29:14 +0800

    Update IPay88Module.java

--- android/src/main/java/com/ipay88/IPay88Module.java
+++ android/src/main/java/com/ipay88/IPay88Module.java
@@ -108,7 +108,7 @@ public class IPay88Module extends ReactContextBaseJavaModule {
             // No need to implement
         }
 
-        public void onConnectionError (String s, String s1, String s2, String s3, String s4, String s5, String s6)
+        public void onConnectionError (String s, String s1, String s2, String s3, String s4, String s5)
         {
             // No need to implement
         }
